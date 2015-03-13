bc23da4aae39d6efa3916ef1a743bb8540585273
Leandro Ribas <leanribas@gmail.com>
2015-03-13 18:47:56 -0300

    -Correção dos paths para conteúdos estáticos

--- public_html/index.php
+++ public_html/index.php
@@ -10,8 +10,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!-- Bootstrap -->
-        <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
-        <link href="css/template.css" rel="stylesheet" media="screen">
+        <link href="/css/bootstrap.min.css" rel="stylesheet" media="screen">
+        <link href="/css/template.css" rel="stylesheet" media="screen">
 
         <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
         <!--[if lt IE 9]>
